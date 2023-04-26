0eaa1147e32d7c286444eb9dfba4e3822f0d9f9f
Alexey Ushakov <alexeyushakov16@gmail.com>
2023-04-26 15:59:24 +0300

    Адаптив

---
+++ script.js
@@ -0,0 +1,29 @@
+const popUp = document.querySelector('.popup');
+
+const editButton = document.querySelector('.profile__button_edit');
+
+const closeButton = document.querySelector('.popup__button_close');
+
+const currentName = document.querySelector('.profile__title');
+const formName = document.querySelector('.popup__form_name');
+
+const currentOccupation = document.querySelector('.profile__paragraph');
+const formOccupation = document.querySelector('.popup__form_occupation');
+
+const saveButton = document.querySelector('.popup__button_save');
+
+editButton.addEventListener('click', function () {
+  popUp.classList.toggle('popup_hidden');
+  formName.value = currentName.textContent;
+  formOccupation.value = currentOccupation.textContent;
+})
+
+closeButton.addEventListener('click', function () {
+  popUp.classList.toggle('popup_hidden');
+})
+
+saveButton.addEventListener('click', function() {
+  currentName.textContent = formName.value;
+  currentOccupation.textContent = formOccupation.value;
+  popUp.classList.toggle('popup_hidden');
+})
