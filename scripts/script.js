107f0955e4ef0b2d7f6418533095c2198533728c
Alexey Ushakov <alexeyushakov16@gmail.com>
2023-04-28 20:19:43 +0300

    Пофиксил замечания ревьюера

--- scripts/script.js
+++ scripts/script.js
@@ -5,25 +5,31 @@ const editButton = document.querySelector('.profile__button-edit');
 const closeButton = document.querySelector('.popup__button-close');
 
 const currentName = document.querySelector('.profile__title');
-const formName = document.querySelector('.popup__form_name');
+const formName = document.querySelector('.popup__name');
 
 const currentOccupation = document.querySelector('.profile__paragraph');
-const formOccupation = document.querySelector('.popup__form_occupation');
+const formOccupation = document.querySelector('.popup__occupation');
 
-const saveButton = document.querySelector('.popup__button-save');
+const popUpForm = document.querySelector('.popup__form-container');
+
+const hidePopup = function () {
+  popUp.classList.toggle('popup_hidden');
+}
 
 editButton.addEventListener('click', function () {
-  popUp.classList.toggle('popup_hidden');
+  hidePopup();
   formName.value = currentName.textContent;
   formOccupation.value = currentOccupation.textContent;
 })
 
 closeButton.addEventListener('click', function () {
-  popUp.classList.toggle('popup_hidden');
+  hidePopup();
 })
 
-saveButton.addEventListener('click', function() {
+const handleFormSubmit = function (evt) {
+  evt.preventDefault();
   currentName.textContent = formName.value;
   currentOccupation.textContent = formOccupation.value;
-  popUp.classList.toggle('popup_hidden');
-})
+  hidePopup();
+}
+popUpForm.addEventListener('submit', handleFormSubmit);
