e3160a7662494f4c3758407d2ee2ff192d1c17d2
Alexey Ushakov <alexeyushakov16@gmail.com>
2023-04-29 14:07:04 +0300

    Проверка ПР №3

--- scripts/script.js
+++ scripts/script.js
@@ -5,31 +5,30 @@ const editButton = document.querySelector('.profile__button-edit');
 const closeButton = document.querySelector('.popup__button-close');
 
 const currentName = document.querySelector('.profile__title');
-const formName = document.querySelector('.popup__name');
+const formName = document.querySelector('.popup__field_type_name');
 
 const currentOccupation = document.querySelector('.profile__paragraph');
-const formOccupation = document.querySelector('.popup__occupation');
+const formOccupation = document.querySelector('.popup__field_type_occupation');
 
-const popUpForm = document.querySelector('.popup__form-container');
+const popUpForm = document.querySelector('.popup__form');
 
-const hidePopup = function () {
+const popupVisible = function () {
   popUp.classList.toggle('popup_hidden');
 }
-
-editButton.addEventListener('click', function () {
-  hidePopup();
-  formName.value = currentName.textContent;
-  formOccupation.value = currentOccupation.textContent;
-})
-
-closeButton.addEventListener('click', function () {
-  hidePopup();
-})
 
 const handleFormSubmit = function (evt) {
   evt.preventDefault();
   currentName.textContent = formName.value;
   currentOccupation.textContent = formOccupation.value;
-  hidePopup();
+  popupVisible();
 }
+
+editButton.addEventListener('click', function () {
+  popupVisible();
+  formName.value = currentName.textContent;
+  formOccupation.value = currentOccupation.textContent;
+})
+
+closeButton.addEventListener('click', popupVisible);
+
 popUpForm.addEventListener('submit', handleFormSubmit);
