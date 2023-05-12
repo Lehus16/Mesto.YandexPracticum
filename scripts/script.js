3329234b3520a10ae2b0b4ce307cf8656d5b9cda
Alexey Ushakov <alexeyushakov16@gmail.com>
2023-05-12 11:47:19 +0300

    Переделал закрузку 6 карточек из 'коробки' и event listener для добавления карточки

--- scripts/script.js
+++ scripts/script.js
@@ -24,23 +24,29 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
   },
 ];
+
 // ELEMENTS
 const elements = document.querySelector('.elements');
 const elementImages = document.querySelectorAll('.element__image');
 const elementParagraphs = document.querySelectorAll('.element__paragraph');
 const elementButtons = document.querySelectorAll('.element__button');
-// Загрузка карточек из массива объектов(базы данных). Реализовал циклом(?) через отдельную функцию, чтобы можно было вызывать её в других функциях при необходимости. Если в перспективе повторное использование не понадобиться можно переделать на IIFE.
-const displayElements = function () {
-  for (let i = 0; i < elementImages.length; i++) {
-    elementImages[i].setAttribute('src', initialCards[i].link);
-    elementImages[i].setAttribute('alt', initialCards[i].name);
-    elementParagraphs[i].textContent = initialCards[i].name;
-    elementButtons[i].addEventListener('click', function (e) {
-      elementButtons[i].style.background = 'url("images/BlackLike.svg")';
-    });
+// Загрузка карточек из массива объектов(базы данных).
+    const addNewElement = function (elem, index, array) {
+      const templateElement = document.querySelector('#element').content;
+      const newElement = templateElement.querySelector('.element').cloneNode(true);
+      const newElementImage = newElement.querySelector('.element__image');
+      const newElementParagraph = newElement.querySelector('.element__paragraph');
+      const newElementButton = newElement.querySelector('.element__button');
+      newElementImage.src = elem.link;
+      newElementImage.alt = elem.name;
+      newElementParagraph.textContent = elem.name;
+      newElementButton.addEventListener('click', function() {
+        newElementButton.style.background = 'url("images/BlackLike.svg")';
+    })
+    elements.append(newElement);
+// Хотел сделать функцию, которую вызвал бы в обработчике события при добавлении новой картинки, но отличается append и prepend.
   }
-}
-displayElements();
+    initialCards.forEach(addNewElement);
 
 // Popup редактирования имени и рода деятельности.
 const popupEdit = document.querySelector('.popup-edit');
@@ -94,19 +100,26 @@ addButton.addEventListener('click', addPopupVisible);
 // Callback для добавления нового элемента
 const handleAddFormSubmit = function (evt) {
   evt.preventDefault();
+  initialCards.unshift({name: formPlaceName.value, link: formUrl.value});
   const templateElement = document.querySelector('#element').content;
   const newElement = templateElement.querySelector('.element').cloneNode(true);
-  newElement.querySelector('.element__image').src = `${formUrl.value}`;
-  newElement.querySelector('.element__image').alt = `${formPlaceName.value}`;
-  newElement.querySelector('.element__paragraph').textContent = `${formPlaceName.value}`;
-  newElement.querySelector('.element__button').addEventListener('click', function (){
-    newElement.querySelector('.element__button').style.background = 'url("images/BlackLike.svg")'
-  });
+  const newElementImage = newElement.querySelector('.element__image');
+  const newElementParagraph = newElement.querySelector('.element__paragraph');
+  const newElementButton = newElement.querySelector('.element__button');
+  newElementImage.src = initialCards[0].link;
+  newElementImage.alt = initialCards[0].name;
+  newElementParagraph.textContent = initialCards[0].name;
+  newElementButton.addEventListener('click', function() {
+  newElementButton.style.background = 'url("images/BlackLike.svg")';
+    })
   elements.prepend(newElement);
   addPopupVisible();
   formPlaceName.value = '';
   formUrl.value = '';
+  console.log(initialCards);
 }
+
+
 // Добавление нового элемента.
 addPopupForm.addEventListener('submit', handleAddFormSubmit);
 // Закрытие popup
